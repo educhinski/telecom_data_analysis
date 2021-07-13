eb1c77f6938ac04eae213074cd4f12d8f3ec69f3
Daniel <danielzelalemheru@gmail.com>
2021-07-13 16:36:18 +0300

    CleanTelecomData moved to scripts folder

--- scripts/clean_telecom_df.py
+++ scripts/clean_telecom_df.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 from helper import TelecomHelper
+
+
 class CleanTelecomData:
     def __init__(self, df: pd.DataFrame):
         self.df = df
@@ -28,8 +30,22 @@ class CleanTelecomData:
 
     def drop_columns_with_null_values(self, df: pd.DataFrame, threshold_in_percent=30) -> pd.DataFrame:
         Helper = TelecomHelper()
-        
-        overall_missing = Helper.percent_missing(self.df)
+
+        null_percent_df = pd.DataFrame(columns=['column', 'null_percent'])
+        columns = df.columns.values.tolist()
+
+        null_percent_df['column'] = columns
+        null_percent_df['null_percent'] = null_percent_df['column'].map(
+            lambda x: Helper.percent_missing_for_col(df, x))
+
+        columns_to_be_dropped = null_percent_df[null_percent_df['null_percent']
+                                                > threshold_in_percent]['column'].to_list()
+        df = self.__drop_columns(df, columns_to_be_dropped)
+
+        return df
+
+    def drop_rows_with_null_values(self, df: pd.DataFrame, threshold_in_percent=1) -> pd.DataFrame:
+        Helper = TelecomHelper()
 
         null_percent_df = pd.DataFrame(columns=['column', 'null_percent'])
         columns = df.columns.values.tolist()
@@ -38,11 +54,44 @@ class CleanTelecomData:
         null_percent_df['null_percent'] = null_percent_df['column'].map(
             lambda x: Helper.percent_missing_for_col(df, x))
         
-        columns_to_be_dropped = null_percent_df[null_percent_df['null_percent']
-                                                > threshold_in_percent]['column'].to_list()
-        df = self.__drop_columns(df, columns_to_be_dropped)
+        columns_subset = null_percent_df[null_percent_df['null_percent']
+                                         < threshold_in_percent]['column'].to_list()
+
+        df = df.dropna(subset=columns_subset)
 
         return df
+
+    def handle_missing_qantitative_data_with_mean(self, df: pd.DataFrame, method="mean"):
+
+        numeric_data = ['int16', 'int32', 'int64',
+                        'float16', 'float32', 'float64']
+
+        all_cols = df.columns.to_list()
+        num_cols = [c for c in all_cols if df[c].dtypes in numeric_data]
+
+        if (method == "mean"):
+
+            for col in num_cols:
+                df[col] = df[col].fillna(df[col].mean())
+
+            return df
+
+        elif method == "ffill":
+
+            for col in num_cols:
+                df[col] = df[col].fillna(method='ffill')
+
+            return df
+
+        elif method == "bfill":
+
+            for col in num_cols:
+                df[col] = df[col].fillna(method='bfill')
+
+            return df
+        else:
+            print("Method unknown")
+            return df
 
     def convert_to_mega_bytes(self, df):
 
